c4184af8fbe884885e02856e582dc647b22f664a
Felix McDonagh STUDENT <Felix.McDonagh@cic.ac>
2019-11-07 12:19:10 +0000

    I watch inflation porn

---
+++ XO/Module1.vb
@@ -0,0 +1,51 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim LevelPlace As String
+        Dim RowClip As String
+        Dim move As String
+        Dim RoomX As Integer = "1"
+        Dim RoomY As Integer = "1"
+        Dim Column As Integer = 2
+        Dim Row As Integer = 3
+        Dim Continoo = True
+        Dim LevelFriend As IO.StreamReader
+        Dim Board(,) As String
+        'Dimming space ^^^
+        Do
+            setBOARD(Board, Column, Row, LevelFriend, RoomX, RoomY, RowClip, LevelPlace)
+
+            For y = 0 To 5
+                For x = 0 To 5
+                    Console.Write(Board(y, x))
+                Next
+                Console.WriteLine()
+            Next
+            Console.WriteLine("input your move")
+            move = Console.ReadLine()
+            If move = "W" Then Row = Row - 1
+            If move = "S" Then Row = Row + 1
+            If move = "D" Then Column = Column + 1
+            If move = "A" Then Column = Column - 1
+        Loop Until Continoo = False
+    End Sub
+    Sub setBOARD(ByRef Board, Column, Row, LevelFriend, RoomX, RoomY, RowClip, LevelPlace)
+        LevelPlace = RoomX & " , " & RoomY
+        LevelFriend = LevelPlace
+        LevelFriend = New IO.StreamReader("1,1.txt")
+        Dim Segment As Integer
+        Dim Line As Integer
+        For Line = 0 To 9
+            RowClip = LevelFriend.Readline
+            For Segment = 0 To 9
+
+                Board(Segment, Line) = RowClip(Segment)
+
+            Next
+            Line = 0
+        Next
+        Board(Row, Column) = "H"
+        Console.Clear()
+
+    End Sub
+End Module
---
+++ XO/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("XO")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("XO")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3d659479-890d-44e7-8c21-f5242a4fa574")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
